1137ee33877516d6a9b1ae555f87970a8f8cf7b8
Lukas Mota (Vs) <lukas_motta@outlook.com>
2024-05-15 23:30:45 -0300

    CRUD (Completo)

--- Data/Contexto.cs
+++ Data/Contexto.cs
@@ -1,14 +1,13 @@
-﻿using Crud_Aspdotnet_MVC.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Crud_Aspdotnet_MVC.Models;
 
 namespace Crud_Aspdotnet_MVC.Data
 {
     public class Contexto :DbContext
     {
 
-#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
-        public Contexto(DbContextOptions<Contexto>options)
-#pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
+
+        public Contexto(DbContextOptions<Contexto> options)
 
             : base(options)
         { }
